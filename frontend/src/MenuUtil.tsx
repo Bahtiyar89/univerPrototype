8c60e7082de91ef88c60bff7453c8c55ff545846
BahtiyarIst <99660697+BahtiyarIst@users.noreply.github.com>
2023-11-06 10:03:45 +0300

    added background color

--- frontend/src/MenuUtil.tsx
+++ frontend/src/MenuUtil.tsx
@@ -1,164 +1,128 @@
-
 export const Menus: any = [
   {
-    id: 'dashboard',
-    label: 'Dashboard',
-    href: '/dashboard'
+    id: "dashboard",
+    label: "Dashboard",
+    href: "/dashboard",
   },
   {
-    id: 'hr',
-    label: 'HR',
-    href: '/hr'
+    id: "hr",
+    label: "HR",
+    href: "/hr",
   },
   {
-    id: 'whouse',
-    label: 'Склад',
-    href: '/whouse'
+    id: "whouse",
+    label: "Склад",
+    href: "/whouse",
   },
-  {
-    id: 'marketing',
-    label: 'Маркетинг',
-    href: '/marketing'
-  },
-  {
-    id: 'finance',
-    label: 'Финансы',
-    href: '/finance'
-  },
-  {
-    id: 'service',
-    label: 'Сервис',
-    href: '/service'
-  },
-  {
-    id: 'crm',
-    label: 'CRM',
-    href: '/crm'
-  },
-  {
-    id: 'documents',
-    label: 'Документы',
-    href: '/documents'
-  },
-  {
-    id: 'report',
-    label: 'Отчеты',
-    href: '/report'
-  },
-  {
-    id: 'reference',
-    label: 'Справочники',
-    href: '/reference'
-  }
 ];
 
 export const SubMenus: any = {
-  'hr': [
+  hr: [
     {
-      id: 'employees',
-      label: 'Список сотрудников',
-      href: '/employees'
+      id: "employees",
+      label: "Список сотрудников",
+      href: "/employees",
     },
     {
-      id: 'employees-new',
-      label: 'Новые добавленные',
-      href: '/employees-new'
+      id: "employees-new",
+      label: "Новые добавленные",
+      href: "/employees-new",
     },
     {
-      id: 'employees-all',
-      label: 'Список всех сотрудников',
-      href: '/employees-all'
+      id: "employees-all",
+      label: "Список всех сотрудников",
+      href: "/employees-all",
     },
     {
-      id: 'structure',
-      label: 'Структура',
-      href: '/structure'
-    }
+      id: "structure",
+      label: "Структура",
+      href: "/structure",
+    },
   ],
-  'whouse': [
+  whouse: [
     {
-      id: 'supplies',
-      label: 'Поступления',
-      href: '/supplies'
+      id: "supplies",
+      label: "Поступления",
+      href: "/supplies",
     },
     {
-      id: 'shipments',
-      label: 'Реализации',
-      href: '/shipments'
+      id: "shipments",
+      label: "Реализации",
+      href: "/shipments",
     },
     {
-      id: 'move-outs',
-      label: 'Отправки',
-      href: '/move-outs'
+      id: "move-outs",
+      label: "Отправки",
+      href: "/move-outs",
     },
     {
-      id: 'move-ins',
-      label: 'Приемки',
-      href: '/move-ins'
+      id: "move-ins",
+      label: "Приемки",
+      href: "/move-ins",
     },
     {
-      id: 'returns',
-      label: 'Возвраты',
-      href: '/returns'
+      id: "returns",
+      label: "Возвраты",
+      href: "/returns",
     },
     {
-      id: 'loss',
-      label: 'Списания',
-      href: '/loss'
-    }
+      id: "loss",
+      label: "Списания",
+      href: "/loss",
+    },
   ],
-  'marketing': [
+  marketing: [
     {
-      id: 'contracts',
-      label: 'Договоры',
-      href: '/contracts'
+      id: "contracts",
+      label: "Договоры",
+      href: "/contracts",
     },
     {
-      id: 'gen-contracts',
-      label: 'Общие договоры',
-      href: '/gen-contracts'
+      id: "gen-contracts",
+      label: "Общие договоры",
+      href: "/gen-contracts",
     },
     {
-      id: 'sale-types',
-      label: 'Типы продаж',
-      href: '/sale-types'
+      id: "sale-types",
+      label: "Типы продаж",
+      href: "/sale-types",
     },
     {
-      id: 'contracts-via-bank',
-      label: 'Договоры через банк',
-      href: '/contracts-via-bank'
+      id: "contracts-via-bank",
+      label: "Договоры через банк",
+      href: "/contracts-via-bank",
     },
     {
-      id: 'problem-contracts',
-      label: 'Проблемные',
-      href: '/problem-contracts'
-    }
+      id: "problem-contracts",
+      label: "Проблемные",
+      href: "/problem-contracts",
+    },
   ],
-  'crm': [
+  crm: [
     {
-      id: 'calls-all',
-      label: 'Все звонки',
-      href: '/calls-all'
+      id: "calls-all",
+      label: "Все звонки",
+      href: "/calls-all",
     },
     {
-      id: 'calls-after-sale',
-      label: 'Звонки после продажи',
-      href: '/calls-after-sale'
+      id: "calls-after-sale",
+      label: "Звонки после продажи",
+      href: "/calls-after-sale",
     },
     {
-      id: 'calls-after-service',
-      label: 'Звонки после сервиса',
-      href: '/calls-after-service'
+      id: "calls-after-service",
+      label: "Звонки после сервиса",
+      href: "/calls-after-service",
     },
     {
-      id: 'calls-for-service',
-      label: 'Звонки ЗФ',
-      href: '/calls-for-service'
+      id: "calls-for-service",
+      label: "Звонки ЗФ",
+      href: "/calls-for-service",
     },
     {
-      id: 'calls-for-monthly-payment',
-      label: 'Звонки после взноса',
-      href: '/calls-for-monthly-payment'
+      id: "calls-for-monthly-payment",
+      label: "Звонки после взноса",
+      href: "/calls-for-monthly-payment",
     },
     {
       id: "list-demonstration",
@@ -166,26 +130,26 @@ export const SubMenus: any = {
       href: "/list-demonstration",
     },
   ],
-  'finance': [
+  finance: [
     {
-      id: 'service-payments',
-      label: 'Сервис платежи',
-      href: '/service-payments'
+      id: "service-payments",
+      label: "Сервис платежи",
+      href: "/service-payments",
     },
     {
-      id: 'cash-docs-out',
-      label: 'Расходники',
-      href: '/cash-docs-out'
+      id: "cash-docs-out",
+      label: "Расходники",
+      href: "/cash-docs-out",
     },
     {
-      id: 'cash-moves-out',
-      label: 'Отправки',
-      href: '/cash-moves-out'
+      id: "cash-moves-out",
+      label: "Отправки",
+      href: "/cash-moves-out",
     },
     {
-      id: 'cash-moves-in',
-      label: 'Приемки',
-      href: '/cash-moves-in'
-    }
-  ]
-}
+      id: "cash-moves-in",
+      label: "Приемки",
+      href: "/cash-moves-in",
+    },
+  ],
+};
--- frontend/src/container/Header/AppHeader.tsx
+++ frontend/src/container/Header/AppHeader.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react";
+import { NavLink } from "react-router-dom";
 import CIcon from "@coreui/icons-react";
 import { AppHeaderDropdown } from "./header/index";
 import { logo } from "assets/brand/logo";
@@ -8,44 +9,45 @@ import {
   getModuleNameFromHash,
   getSubmenuIdFromHash,
 } from "../../utils/UriUtil";
-import { cilBell, cilEnvelopeOpen, cilList } from "@coreui/icons";
+import { cilBell, cilEnvelopeOpen, cilList, cilMenu } from "@coreui/icons";
 import {
   CContainer,
   CHeader,
   CHeaderBrand,
+  CHeaderDivider,
   CHeaderNav,
+  CHeaderToggler,
   CNavItem,
   CNavLink,
 } from "@coreui/react-pro";
+import { useDispatch, useSelector } from "react-redux";
 
 type AppHeaderProps = {
   currHash: string;
 };
 
 const AppHeader: FC<AppHeaderProps> = ({ currHash }) => {
-  const menuLocation = getModuleNameFromHash(currHash)
+  const menuLocation = getModuleNameFromHash(currHash);
   const subMenus = SubMenus[menuLocation] || [];
   const submenuId = getSubmenuIdFromHash(currHash);
 
   return (
-    <CHeader position="sticky" className="mb-4">
-      <CContainer fluid style={{ backgroundColor: "#3399ff", width: "100%" }}>
+    <CHeader
+      position="sticky"
+      style={{ paddingRight: "40px" }}
+      className="mb-4"
+    >
+      <CContainer fluid>
         <CHeaderBrand className="mx-auto d-md-none" href="/">
           <CIcon icon={logo} height={48} />
         </CHeaderBrand>
-        <CHeaderNav
-          className="d-none d-md-flex me-auto"
-          style={{ width: "80%", margin: "auto", color: "white" }}
-        >
+        <CHeaderNav className="d-none d-md-flex me-auto">
           {Menus?.map((item: any, index: number) => (
             <CNavItem
               key={index}
               className={menuLocation === item.id ? "active" : ""}
             >
-              <CNavLink
-                style={{ color: "white" }}
-                href={item.href ? `${item.href}` : "/404"}
-              >
+              <CNavLink href={item.href ? `${item.href}` : "/404"}>
                 {item.label}
               </CNavLink>
             </CNavItem>
